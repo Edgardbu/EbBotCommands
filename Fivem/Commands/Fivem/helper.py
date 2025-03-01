3bcdf65b2f734ce4baae9ec055cf5b59628a65ee
Edgard <76914845+Edgardbu@users.noreply.github.com>
2025-03-01 16:07:25 +0200

    Update helper.py
    fivem_replace_variables added

--- Fivem/Commands/Fivem/helper.py
+++ Fivem/Commands/Fivem/helper.py
@@ -1,7 +1,8 @@
 import aiohttp
 import json
 import typing
-
+import discord
+import utils
 
 async def get_player_data(base_url: str, players_endpoint: str) -> typing.Tuple[list, int, typing.Optional[str]]:
     # The base URL for requesting player data
@@ -43,3 +44,17 @@ async def get_server_info(base_url: str, info_endpoint: str) -> typing.Tuple[str
         error_reason = str(e)
 
     return info, status_ok, error_reason
+
+def fivem_replace_variables(text: str, fivem_data: list, member: discord.Member = None, guild: discord.Guild = None) -> str | None:
+    """
+    [server_name, server_ip, players, max_players]
+    """
+    if text is None:
+        return None
+
+    text = text.replace("{{fivem_server_name}}", fivem_data[0])
+    text = text.replace("{{server_ip}}", fivem_data[1])
+    text = text.replace("{{players_length}}", str(len(fivem_data[2])))
+    text = text.replace("{{max_players}}", str(fivem_data[3]))
+
+    return utils.replace_variables(text, member, guild)
